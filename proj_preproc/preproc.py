a67a71a427348e3a4f9d7c6f8d436001e0203b7c
PA-Segura <segurachavez@gmail.com>
2025-03-27 15:04:13 -0600

    update preproc

--- proj_preproc/preproc.py
+++ proj_preproc/preproc.py
@@ -1,9 +1,18 @@
+# %%
+
+import sys
+# sys.path.append('./eoas_pyutils')  # Doesn't work when using a conda env outside home
+sys.path.append('/home/olmozavala/air_pollution_forecast/eoas_pyutils')
+import os
 import numpy as np
+import pickle
 from datetime import datetime, date, timedelta
 import calendar
 import pandas as pd
-from inout.io_common import  create_folder
+from ai_common.constants.AI_params import NormParams
+from sklearn import preprocessing
 from os.path import join
+from pandas import DataFrame
 
 from conf.localConstants import constants
 
@@ -11,6 +20,8 @@ from conf.localConstants import constants
 _min_value_ozone = 0
 _max_value_ozone = 250
 
+
+# %%
 def generate_date_hot_vector(datetimes_original):
     # datetimes = [datetime.strptime(x, '%Y-%m-%d %H:%M:%S') for x in datetimes_original]
     datetimes = datetimes_original
@@ -42,90 +53,74 @@ def generate_date_hot_vector(datetimes_original):
     dates_hv_df = pd.DataFrame(out_dates_hv, columns=column_names, index=datetimes_original)
     return dates_hv_df
 
-def normalizeAndFilterData(data, datetimes_orig, forecasted_hours, output_folder='', run_name='', read_from_file=False):
-    """
-    This function normalizes de data and filters only the cases where we
-    have the appropiate forecasted times. It also obtains the 'y' index
-    :param data: All the data
-    :param datetimes_str: An array of datetimes as strings which correspond to the index
-    :param forecasted_hours: an integer representing the number of hours in advance we want to read
-    :return:
-    """
-    # Predicting for the next value after 24hrs (only one)
-    print("Normalizing data....")
-    datetimes = np.array(datetimes_orig)
+# %%
+def normalizeData(data, norm_type, file_name):
+    if norm_type == NormParams.min_max:
+        scaler = preprocessing.MinMaxScaler()
+    if norm_type == NormParams.mean_zero:
+        scaler = preprocessing.StandardScaler()
 
-    all_data_cols = data.columns.values
-    date_columns = [x for x in all_data_cols if (x.find('week') != -1) or (x.find('hour') != -1) or (x.find('year') != -1)]
-    stations_columns = [x for x in all_data_cols if (x.find('h') == -1) and (x not in date_columns)]
-    meteo_columns = [x for x in all_data_cols if (x.find('h') != -1) and (x not in date_columns)  and (x not in stations_columns)]
+    scaler = scaler.fit(data)
+    data_norm_np = scaler.transform(data)
+    data_norm_df = DataFrame(data_norm_np, columns=data.columns, index=data.index)
 
-    # Normalizing meteorological variables
-    # In this case we obtain the normalization values directly from the data
-    # meteo_names = ['U10', 'V10', 'RAINC', 'T2', 'RAINNC', 'PBLH', 'SWDOWN', 'GLW']
-    meteo_names = ['U10', 'V10', 'RAINC', 'T2', 'RAINNC', 'SWDOWN', 'GLW']
-    if not(read_from_file):
-        min_data = {}
-        max_data = {}
-        for cur_meteo in meteo_names:
-            cur_meteo_cols = [x for x in meteo_columns if x.find(cur_meteo) != -1]
-            min_data[cur_meteo] = data[cur_meteo_cols].min().min()
-            max_data[cur_meteo] = data[cur_meteo_cols].max().max()
-        # ********* Saving normalization values for each variable ******
-        create_folder(output_folder)
-        pd.DataFrame(min_data, index=[1]).to_csv(join(output_folder,F'{run_name}_min_values.csv'))
-        pd.DataFrame(max_data, index=[1]).to_csv(join(output_folder,F'{run_name}_max_values.csv'))
-    else: # In this case we obtain the normalization values from the provided file
-        min_data = pd.read_csv(join(output_folder,F'{run_name}_min_values.csv'), index_col=0)
-        max_data = pd.read_csv(join(output_folder,F'{run_name}_max_values.csv'), index_col=0)
+    # ******************* Saving Normalization params, scaler object **********************
+    scaler.path_file = file_name
+    with open(scaler.path_file, 'wb') as f: #scaler.path_file must be defined during training.
+        pickle.dump(scaler, f)
+    print(f'Scaler/normalizer object saved to: {scaler.path_file}')
+    print(F'Done! Current shape: {data_norm_df.shape} ')
+    return data_norm_df
 
-    data_norm_df = data.copy()
 
-    # Normalizing the meteorological variables
-    for cur_meteo in meteo_names:
-        cur_meteo_cols = [x for x in meteo_columns if x.find(cur_meteo) != -1]
-        # The data structure is a little bit different when reading from the file
-        if not (read_from_file):
-            min_val = min_data[cur_meteo]
-            max_val = max_data[cur_meteo]
-        else:
-            min_val = min_data[cur_meteo].values[0]
-            max_val = max_data[cur_meteo].values[0]
-        data_norm_df[cur_meteo_cols] = (data_norm_df[cur_meteo_cols] - min_val)/(max_val - min_val)
+def loadScaler(file_name):
+    with open(file_name, 'rb') as f:
+        scaler = pickle.load(f)
+    
+    return scaler
 
-    # Normalizing the pollution variables
-    data_norm_df[stations_columns] = (data_norm_df[stations_columns] - _min_value_ozone)/(_max_value_ozone - _min_value_ozone)
-    print(F'Done!')
+#Function to extract model_name from file names TODO: Pasar a proj_fun
+def extract_model_name(models_path):
+    # Get the list of files in the 'models' directory
+    files = os.listdir(models_path)
 
-    # Filtering only dates where there is data "forecasted hours after" (24 hrs after)
-    print(F"Building X and Y ....")
-    accepted_times_idx = []
-    y_times_idx = []
-    for i, c_datetime in enumerate(datetimes):
-        forecasted_datetime = c_datetime + np.timedelta64(forecasted_hours,'h')
-        if forecasted_datetime in datetimes:
-            accepted_times_idx.append(i)
-            y_times_idx.append(np.argwhere(forecasted_datetime == datetimes)[0][0])
+    # Filter only the files with the model name
+    model_files = [file for file in files if file.endswith('.hdf5')]
 
-    # ****************** Replacing nan columns with the mean value of all the other columns ****************
-    mean_values = data_norm_df[stations_columns].mean(1)
+    if len(model_files) > 0:
+        # Extract the model name from the first file (assuming all files have the same name prefix)
+        model_name = model_files[0].split('-epoch')[0]
+        return model_name
+    else:
+        return None
 
-    # Replace nan values with -1 and add additional MEAN column
-    print(F"Filling nan values....")
-    data_norm_df_final = data_norm_df.copy()
-    for cur_station in stations_columns:
-        data_norm_df_final[cur_station] = data_norm_df[cur_station].fillna(-1)
-
-    data_norm_df_final['MEAN'] = mean_values
-
-    # print(F"Norm params: {scaler.get_params()}")
-    # file_name_normparams = join(parameters_folder, F'{model_name}.txt')
-    # utilsNN.save_norm_params(file_name_normparams, NormParams.min_max, scaler)
-    print("Done!")
-
-    return data_norm_df_final, accepted_times_idx, y_times_idx, stations_columns, meteo_columns
 
 def deNormalize(data):
     unnormalize_data = data*(_max_value_ozone- _min_value_ozone) + _min_value_ozone
     return unnormalize_data
 
+
+def apply_bootstrap(X_df, Y_df, contaminant, station, boostrap_threshold, forecasted_hours, boostrap_factor=1):
+    '''
+    This function will boostrap the data based on the threshold and the forecasted hours
+    '''
+
+    bootstrap_column = f"cont_{contaminant}_{station}"
+    print("Bootstrapping the data...")
+    # Searching all the index where X or Y is above the threshold
+
+    # Adding index when the current time is above the threshold
+    bootstrap_idx = X_df.loc[:,bootstrap_column] > boostrap_threshold
+
+    # Searching index when any of the forecasted hours is above the threshold
+    y_cols = Y_df.columns.values
+    for i in range(1, forecasted_hours+1):
+        # print(bootstrap_idx.sum())  
+        c_column = f"plus_{i:02d}_{bootstrap_column}"
+        if c_column in y_cols:
+            bootstrap_idx = bootstrap_idx | (Y_df.loc[:, c_column] > boostrap_threshold)
+
+    X_df = pd.concat([X_df, *[X_df[bootstrap_idx] for i in range(boostrap_factor)]])
+    Y_df = pd.concat([Y_df, *[Y_df[bootstrap_idx] for i in range(boostrap_factor)]])
+
+    return X_df, Y_df
